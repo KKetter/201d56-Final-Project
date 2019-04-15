aeffc86d2a014f20e7219ede84112f498fb9b80b
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-04-15 16:43:33 -0700

    add valid moves function

--- js/app.js
+++ js/app.js
@@ -6,6 +6,23 @@ var boardGrid = document.getElementById('tile-container');
 var divGrid = boardGrid.children;
 var emptyTileColor = 'white';
 var regularTileColor = '#aaa';
+var allValidMoves = [];
+
+function ValidMoves(key, array) {
+  this.key = key;
+  this.moves = array;
+  allValidMoves.push(this);
+}
+
+new ValidMoves(0, [1, 3]);
+new ValidMoves(1, [0, 2, 4]);
+new ValidMoves(2, [1, 6]);
+new ValidMoves(3, [0, 4, 6]);
+new ValidMoves(4, [1, 3, 5, 8]);
+new ValidMoves(5, [2, 4, 8]);
+new ValidMoves(6, [3, 7]);
+new ValidMoves(7, [4, 6, 8]);
+new ValidMoves(8, [5, 7]);
 
 //Function to generate random numbers to board array
 function generateRandomNumber(){
@@ -17,12 +34,10 @@ function generateRandomNumber(){
       board.push(random);
     }
   }
-
 }
 
 //function to place random numbers to the board
 function drawBoard(){
-  
   for (var i = 0; i < divGrid.length; i++){
     divGrid[i].innerHTML = board[i];
 
@@ -32,13 +47,17 @@ function drawBoard(){
     } else{
       divGrid[i].style.backgroundColor = regularTileColor;
     }
-    
   }
 }
 
-//TODO
-function validMove(){
-  return true;
+//function to check that tile clicked shares border with empty tile
+function validMove(index, zeroIndex) {
+  if (allValidMoves[index].moves.includes(zeroIndex)) {
+    console.log(true);
+    return true;
+  }
+  console.log(false);
+  return false;
 }
 
 //function to get zero index
@@ -49,18 +68,15 @@ function getZero(){
 //function for event listener
 function handleClick(event){
   //Check if the tile can be moved
-  if (validMove){
-    var select = parseInt(event.target.textContent);
-    var indexOfZero = getZero();
+  var select = parseInt(event.target.textContent);
+  var indexOfZero = getZero();
+  if (validMove(board.indexOf(select), indexOfZero)){
     board[board.indexOf(select)] = 0;
     board[indexOfZero] = select;
     drawBoard();
   }
-
 }
-
 
 generateRandomNumber();
 drawBoard();
 boardGrid.addEventListener('click', handleClick);
-
