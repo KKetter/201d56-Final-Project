3e6f43f05d20ec53b4ca8f1a41181b5b81bfd317
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-04-15 14:01:24 -0700

    Generate random numbers for the board

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log('yay team bitwise');
+var board = [];
+var grid = 9;
+
+//Function to generate random numbers to board array
+function generateRandomNumber(){
+  var random = Math.floor(Math.random() * grid);
+  board.push(random);
+  while(board.length < grid){
+    random = Math.floor(Math.random() * grid);
+    if(!board.includes(random)){
+      board.push(random);
+    }
+  }
+
+}
+
+//function to place random numbers to the board
+function initializeBoard(){
+  var divGrid = document.getElementById('tile-container').children;
+  for (var i = 0; i < divGrid.length; i++){
+    divGrid[i].innerHTML = board[i];
+    
+  }
+}
+
+
+
+
+
+
+
+generateRandomNumber();
+initializeBoard();
