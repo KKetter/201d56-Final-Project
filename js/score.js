69243f7490440f39509ca9b59e7f90d9c4e46d1c
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-04-17 15:33:52 -0700

    finish high score

--- js/score.js
+++ js/score.js
@@ -2,25 +2,41 @@
 // global variables
 var scoreBase = [];
 
+if (localStorage.gameInfo){
+  var gameInfo = JSON.parse(localStorage.gameInfo);
+  localStorage.clear('gameInfo');
+  checkHighScore(gameInfo[0], gameInfo[1]);
+} else {
+  loadHighScore();
+}
+
 function checkHighScore(user, userScore){
   if (localStorage.scores){
     var highScore = JSON.parse(localStorage.scores);
-    console.log(highScore);
     scoreBase = highScore;
   }
   scoreBase.push({userName: user, score: userScore});
+}
+
+function loadHighScore(){
+  if (localStorage.scores){
+    var highScore = JSON.parse(localStorage.scores);
+    scoreBase = highScore;
+  }
 }
 
 function saveHighScores(){
   if (scoreBase.length > 10){scoreBase.pop();}
   localStorage.setItem('scores', JSON.stringify(scoreBase));
 }
+
 // function to sort object. from MDN Array.prototype.sort
 function compare(a,b){
   if (a.score < b.score) return -1;
   if (a.score > b.score) return 1;
   return 0;
 }
+
 function renderScores(){
   var targetEl = document.getElementById('highScore');
   for (let i = 0; i < scoreBase.length; i++){
@@ -29,6 +45,7 @@ function renderScores(){
     targetEl.appendChild(liEl);
   }
 }
+
 scoreBase.sort(compare);
 saveHighScores();
 renderScores();
