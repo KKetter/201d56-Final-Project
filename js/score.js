ad8f34ca8b422b696a8bf896d39c48d2c63416e7
Cristian Restrepo <crestrepo21@gmail.com>
2019-04-17 14:01:59 -0700

    scores rendered

--- js/score.js
+++ js/score.js
@@ -12,6 +12,7 @@ function checkHighScore(user, userScore){
 }
 
 function saveHighScores(){
+  if (scoreBase.length > 10){scoreBase.pop();}
   localStorage.setItem('scores', JSON.stringify(scoreBase));
 }
 // function to sort object. from MDN Array.prototype.sort
@@ -20,6 +21,16 @@ function compare(a,b){
   if (a.score > b.score) return 1;
   return 0;
 }
-checkHighScore('jorie',10);
+function renderScores(){
+  var targetEl = document.getElementById('highScore');
+  for (let i = 0; i < scoreBase.length; i++){
+    var liEl = document.createElement('li');
+    liEl.innerHTML = `<span class="scoreDisplay">${scoreBase[i].userName}</span> <span class="scoreDisplay">${scoreBase[i].score}</span>`;
+    targetEl.appendChild(liEl);
+  }
+}
+
+checkHighScore('anthony',1);
 scoreBase.sort(compare);
 saveHighScores();
+renderScores();
